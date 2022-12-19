c7e2cc2244177e90252b2c805a0cba91c768e816
Marnie Lindenthal <Marnie.Lindenthal@web.de>
2022-12-19 13:14:04 +0100

    added json datadase

---
+++ data.js
@@ -0,0 +1,44 @@
+let userprofil = [{
+        name: "Sansas_Papa",
+        picture: "",
+        id: 1
+    }, {
+        name: "Mattis-Mama",
+        picture: "",
+        id: 2
+    },
+    {
+        name: "MarlasPapa",
+        picture: "",
+        id: 3
+    },
+    {
+        name: "FlummiMarla",
+        picture: "",
+        id: 4
+    },
+    {
+        name: "SchmollnaseMattis",
+        picture: "",
+        id: 5
+    },
+    {
+        name: "KleinSansa",
+        picture: "",
+        id: 6
+    },
+    {
+        name: "Lilly_und_Loki",
+        picture: "",
+        id: 7
+    }
+]
+
+let posts = [{
+    autor: 1,
+    picture: ,
+    location: ,
+    likes: ,
+    comments: [{ name: , post: }]
+    comment:
+}]
