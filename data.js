cf9ef8fdf25d5ec1dced32e07499a302e47dde71
Marnie Lindenthal <Marnie.Lindenthal@web.de>
2022-12-21 20:13:34 +0100

    prototyp comment

--- data.js
+++ data.js
@@ -1,44 +1,45 @@
 let userprofil = [{
         name: "Sansas_Papa",
-        picture: "",
+        picture: "mama_und_papa.jpg",
         id: 1
     }, {
         name: "Mattis-Mama",
-        picture: "",
+        picture: "marnie.jpg",
         id: 2
     },
     {
         name: "MarlasPapa",
-        picture: "",
+        picture: "flo.jpg",
         id: 3
     },
     {
         name: "FlummiMarla",
-        picture: "",
+        picture: "marla.jpg",
         id: 4
     },
     {
         name: "SchmollnaseMattis",
-        picture: "",
+        picture: "mama_mattis.jpg",
         id: 5
     },
     {
         name: "KleinSansa",
-        picture: "",
+        picture: "sansa.jpg",
         id: 6
     },
     {
         name: "Lilly_und_Loki",
-        picture: "",
+        picture: "lilly_und_loki.jpg",
         id: 7
     }
 ]
 
 let posts = [{
     autor: 1,
-    picture: ,
-    location: ,
-    likes: ,
+    picture: 'papa_sansa.jpg',
+    location: "Kos",
+    likes: 34,
+    infotext: "Guten Morgen aus dem Urlaub. Sansa ist begeistert.",
     comments: [{ name: , post: }]
     comment:
 }]
