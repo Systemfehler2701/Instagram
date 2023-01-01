5b82bbf8f3851efb672c70f06f7c508f155a6f32
Marnie Lindenthal <Marnie.Lindenthal@web.de>
2023-01-01 13:32:35 +0100

    included render post function

--- data.js
+++ data.js
@@ -1,45 +1,130 @@
-let userprofil = [{
-        name: "Sansas_Papa",
+let userprofiles = [{
+        name: "Nicht gefunden",
+        picture: "not_found.jpg",
+        id: 0
+    }, {
+        name: "Sansas Papa",
         picture: "mama_und_papa.jpg",
         id: 1
     }, {
-        name: "Mattis-Mama",
+        name: "Mattis Mama",
         picture: "marnie.jpg",
         id: 2
     },
     {
-        name: "MarlasPapa",
+        name: "Marlas Papa",
         picture: "flo.jpg",
         id: 3
     },
     {
-        name: "FlummiMarla",
+        name: "Flummi-Marla",
         picture: "marla.jpg",
         id: 4
     },
     {
-        name: "SchmollnaseMattis",
+        name: "Schmollnase Mattis",
         picture: "mama_mattis.jpg",
         id: 5
     },
     {
-        name: "KleinSansa",
+        name: "Klein-Sansa",
         picture: "sansa.jpg",
         id: 6
     },
     {
-        name: "Lilly_und_Loki",
+        name: "Lilly und Loki",
         picture: "lilly_und_loki.jpg",
         id: 7
     }
 ]
 
 let posts = [{
-    autor: 1,
-    picture: 'papa_sansa.jpg',
-    location: "Kos",
-    likes: 34,
-    infotext: "Guten Morgen aus dem Urlaub. Sansa ist begeistert.",
-    comments: [{ name: , post: }]
-    comment:
-}]
+        autor: 1,
+        picture: 'papa_sansa2.jpg',
+        location: "Peine",
+        likes: 23,
+        text: "Frühe Erziehung zum Zocker und Programmierer.",
+        comments: [{ name: 2, post: "Das müssen die Gene sein...." }, { name: 4, post: "Richtig so!" }, { name: 7, post: "miau" }]
+    },
+    {
+        autor: 2,
+        picture: 'mama_marla_mattis.jpg',
+        location: "Bodrum",
+        likes: 56,
+        text: "Mit meinen beiden Großen den Ausflug in die Türkei genießen.",
+        comments: [{ name: 5, post: "Unser schönster Urlaub" }, { name: 4, post: "Und Mama ganz entspannt." }, { name: 7, post: "mau" }, { name: 3, post: "Schönen Urlaub" }]
+    },
+    {
+        autor: 3,
+        picture: 'marla_prüfung.jpg',
+        location: "Stederdorf",
+        likes: 125,
+        text: "Mein Mädchen hat ihre erste Prüfung im Jujutsu gemeistert. Bin stolz auf dich.",
+        comments: [{ name: 1, post: "Wir sind alle stolz auf dich!" }, { name: 2, post: "Mein großes Mädchen..." }, { name: 6, post: "Wenn ich groß bin will ich so sein wie du." }, { name: 5, post: "Große Schwester" }, { name: 7, post: "maumiau" }, { name: 4, post: "Danke" }]
+    },
+    {
+        autor: 4,
+        picture: 'marla_halloween.jpg',
+        location: "Peine",
+        likes: 42,
+        text: "Was sagen wir dem Tod wenn wir ihm begegnen?",
+        comments: [{ name: 2, post: "Nicht heute!" }, { name: 3, post: "Immer diese Serienjunkies..." }]
+    },
+    {
+        autor: 5,
+        picture: 'ironmattis.jpg',
+        location: "",
+        likes: 55,
+        text: "Ironman is in the house!!!",
+        comments: [{ name: 4, post: "Mein Bruder der Held" }, { name: 2, post: "Ich bin dein größter Fan!!!" }, { name: 7, post: "miau" }, { name: 1, post: "Kleiner Superheld" }]
+    },
+    {
+        autor: 6,
+        picture: 'faultier.jpg',
+        location: "Vöhrum",
+        likes: 62,
+        text: "So klein und sooooo faul.",
+        comments: [{ name: 3, post: "Oder entspannt" }, { name: 7, post: "miau" }, { name: 2, post: "Ganz der Papa" }, { name: 1, post: "Hahaha" }]
+    },
+    {
+        autor: 7,
+        picture: 'lilly_und_loki2.jpg',
+        location: "Peine",
+        likes: 34,
+        text: "KatzeKatze",
+        comments: [{ name: 1, post: "Auf meinem Stuhl" }, { name: 3, post: "Fellnasen" }, { name: 2, post: "Flauschalarm!" }]
+    },
+    {
+        autor: 3,
+        picture: 'flo_marla.jpg',
+        location: "Stederdorf",
+        likes: 72,
+        text: "Vater und Tochter.",
+        comments: [{ name: 2, post: "Hier sehen sie ein Flöchen beim aufziehen seines Nachwuchses." }]
+    },
+
+    {
+        autor: 1,
+        picture: 'papa_sansa.jpg',
+        location: "Kos",
+        likes: 34,
+        text: "Guten Morgen aus dem Urlaub. Sansa ist begeistert.",
+        comments: [{ name: 7, post: "miau" }, { name: 2, post: "Mit Morgenkaffee auf dem Balkon" }, { name: 3, post: "und ich nicht dabei.." }, { name: 4, post: "und jeden Tag Eis und Pool" }, { name: 5, post: "und essen was man mag" }, { name: 5, post: "Spielen im Kinderland" }]
+    },
+    {
+        autor: 4,
+        picture: 'marla_sansa.jpg',
+        location: "Vöhrum",
+        likes: 52,
+        text: "Meine kleine Schwester und ich.",
+        comments: [{ name: 1, post: "Meine beiden Mädels" }, { name: 7, post: "miau" }, { name: 5, post: "Meine Schwestern" }, { name: 6, post: "Bald nicht mehr die Kleinste" }]
+    },
+    {
+        autor: 5,
+        picture: 'mattis_halloween.jpg',
+        location: "Vöhrum",
+        likes: 38,
+        text: "Kürbiskopf",
+        comments: [{ name: 3, post: "Erschrecken hohl" }, { name: 7, post: "miau mio miau mio" }, { name: 2, post: "sweet" }]
+    }
+]
--- script.js
+++ script.js
@@ -2,3 +2,84 @@ function dayOrNight() {
     let element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function renderPost() {
+    let content = document.getElementById('content-posts');
+    content.innerHTML = '';
+
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+        let autor = getUserFromId(post.autor);
+        generatePost(content, post, autor);
+    }
+}
+
+function generatePost(content, post, autor) {
+    content.innerHTML += `
+        <div class="post">
+            ${generatePostHeader(post.location, autor)}
+            ${generatePostPicture(post.picture)}
+            ${generateActionButtons()}
+            ${generateLikes(post.likes)}    
+            ${generatePostText(post.text, autor.name)}
+            ${generatePostComment()}          
+        </div>`;
+}
+
+function generatePostHeader(postLocation, userProfile) {
+    return `<div class="post-header">
+                <div class="post-profilpicture"><img src="pictures/${userProfile.picture}"></div>
+                <div class="post-info">
+                    <div class="post-profilname"><span>${userProfile.name}</span></div>
+                    <div class="profil-location"><span>${postLocation}</span></div>
+                </div>
+            </div>`;
+}
+
+function generatePostPicture(postPicture) {
+    return `<div class="post-picture"><img src="pictures/${postPicture}">
+    </div>`;
+}
+
+function generateActionButtons() {
+    return `<div class="post-content">
+                <div class="post-menu">
+                    <div class="post-action">
+                        <img src="png/heart.png">
+                        <img src="png/plaudern.png">
+                        <img src="png/paper-airplane.png">
+                    </div>
+                    <img src="png/goal.png">
+                </div>`;
+}
+
+function generateLikes(postLikes) {
+    return `<div class="post-like">Gefällt <span>${postLikes}</span> mal
+    </div>`;
+}
+
+function generatePostText(postText, autorName) {
+    return ` <div class="post-infotext">               
+    <span class="post-autorname">${autorName} </span>
+    <span class="post-text">${postText}</span>
+</div>
+</div>`;
+}
+
+function generatePostComment() {
+    return `<div class="post-comment">
+                <img src="png/happiness.png">
+                <div contenteditable data-placeholder="Kommentieren..."></div>
+                <button>Posten</button>
+            </div>`
+}
+
+function getUserFromId(userId) {
+    let itemsFound = userprofiles.filter(function(element) {
+        return element.id == userId;
+    });
+    if (itemsFound.length == 1) {
+        return itemsFound[0];
+    }
+    return getUserFromId(0);
+}
